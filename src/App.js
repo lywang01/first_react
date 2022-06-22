1ed68d050f5af672775a4aaa108c7b66261f4dea
lywang01 <lywang@gmail.com>
2022-06-22 16:57:15 -0400

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
 
-function App() {
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="Sara" />
+//       <Welcome name="Cahal" />
+//       <Welcome name="abc artbb" />
+//     </div>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+
+function Avatar(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img className = "Avatar"
+    src={props.user.avatarUrl}
+    alt={props.user.name} />
+  );
+  
+}
+
+function UserInfo(props){
+  return(
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}
+      </div>
     </div>
+
+  );
+}
+
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+     <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+
+function App(){
+  return(
+    <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />
   );
 }
 
